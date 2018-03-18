860259cb5a67f1bf7fe7bcaa2ef4432448b0da0e
Sohil Shah <shah.sohil123@gmail.com>
2018-03-17 22:18:57 -0400

    Initial COmmit

---
+++ app/src/main/java/com/example/shahsohil123/imagecapture/ImageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shahsohil123.imagecapture
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image)
+    }
+}
